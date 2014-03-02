5894ecba0d43a8650e68f082745b884a69404758
soroa <soroa@student.ethz.ch>
2014-03-02 15:13:42 +0100

    test to check functioning of github connection

---
+++ ObeSense/ObeSense_GIThub_test_class.h
@@ -0,0 +1,13 @@
+//
+//  ObeSense_GIThub_test_class.h
+//  ObeSense
+//
+//  Created by Andrea Soro on 02.03.14.
+//  Copyright (c) 2014 Andrea Soro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ObeSense_GIThub_test_class : UIDynamicBehavior
+
+@end
---
+++ ObeSense/ObeSense_GIThub_test_class.m
@@ -0,0 +1,13 @@
+//
+//  ObeSense_GIThub_test_class.m
+//  ObeSense
+//
+//  Created by Andrea Soro on 02.03.14.
+//  Copyright (c) 2014 Andrea Soro. All rights reserved.
+//
+
+#import "ObeSense_GIThub_test_class.h"
+
+@implementation ObeSense_GIThub_test_class
+
+@end
